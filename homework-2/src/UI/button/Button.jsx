f67e13daf035753363a754d034a56c158f138fca
Temirlan <askarovtemirlan29@.gmail.com>
2023-01-09 18:05:31 +0600

    added text

--- homework-2/src/UI/button/Button.jsx
+++ homework-2/src/UI/button/Button.jsx
@@ -4,7 +4,7 @@ import "./Button.css"
 const Button = (props) => {
   return (
     <button className="myBtn" onClick={props.onClick}>
-      {props.title}
+      {props.title} fhsdkad
     </button>
   );
 };
